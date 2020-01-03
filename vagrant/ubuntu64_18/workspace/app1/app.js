188152098c3e8521748ddb09e796cd81dc041f61
tamotsu ichiba <ichibat@gmail.com>
2020-01-04 08:44:02 +0900

    improved

--- vagrant/ubuntu64_18/workspace/app1/app.js
+++ vagrant/ubuntu64_18/workspace/app1/app.js
@@ -3,6 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+//New Code
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/app1db');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +26,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// accessing database
+app.use((req,res,next) => {
+  req.db = db;
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- vagrant/ubuntu64_18/workspace/app1/routes/index.js
+++ vagrant/ubuntu64_18/workspace/app1/routes/index.js
@@ -6,8 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/helloworld', (req,res) => {
-  res.render('helloworld', {title: 'Hello, World!'});
+router.get('/helloworld', (req, res) => {
+  res.render('helloworld', {title: 'Hello, Worlds!'});
+});
+
+/* user list htmlページを作成 */
+router.get('/userlist', (req,res) => {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, (e, docs) => {
+    res.render('userlist', {
+      "userlist": docs
+    });
+  });
 });
 
 module.exports = router;
